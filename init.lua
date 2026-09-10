7666929029899b723705fbc2ae3269bb09ba359c
myip <michaelyipchenmz@gmail.com>
2026-09-09 21:06:10 -0600

    nvimtree, and other mappings

--- init.lua
+++ init.lua
@@ -110,7 +110,7 @@ do
   vim.o.number = true
   -- You can also add relative line numbers, to help with jumping.
   --  Experiment for yourself to see if you like it!
-  -- vim.o.relativenumber = true
+  vim.o.relativenumber = true
 
   -- Enable mouse mode, can be useful for resizing splits for example!
   vim.o.mouse = 'a'
@@ -233,6 +233,8 @@ do
   vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
   vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
   vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+  vim.keymap.set('i', 'jk', '<Esc>', { silent = true })
+  vim.keymap.set('n', '<leader>e', ':NvimTreeFocus<CR>', { desc = 'NvimTreeFocus' })
 
   -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
   -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
@@ -784,6 +786,18 @@ do
     gh 'mason-org/mason.nvim',
     gh 'mason-org/mason-lspconfig.nvim',
     gh 'WhoIsSethDaniel/mason-tool-installer.nvim',
+  }
+
+  vim.pack.add {
+    { src = 'https://github.com/nvim-tree/nvim-web-devicons' }, -- optional
+    { src = 'https://github.com/nvim-tree/nvim-tree.lua' },
+  }
+  require('nvim-tree').setup {
+    view = {
+      adaptive_size = true,
+      number = true,
+      relativenumber = true,
+    },
   }
 
   -- Automatically install LSPs and related tools to stdpath for Neovim
